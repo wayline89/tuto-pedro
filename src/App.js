fe0917d1a847a4fc94f0fa26e226e58dda1ed6c0
wayline89 <laurentdugardin@gmail.com>
2024-11-20 16:48:18 +0100

    tuto-react-pedro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showText, setShowText] = useState(true);
+
   return (
     <div className="App">
-      <h1>test h1 </h1>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show / hide
+      </button>
+      {showText === true && <h1>hi my name is jhfk</h1>}
     </div>
   );
 }
